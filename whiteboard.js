f549bd9e7444f4c6706dc25d84b9a0e2a4b988ee
kristoffer.snabb <kristoffer.snabb@2a9e65dc-1f86-11df-98cf-3116c4ba2102>
2010-03-09 19:32:40 +0000

    Added the basis html with all the function buttons but no real functions yet

---
+++ whiteboard.js
@@ -0,0 +1,31 @@
+
+
+var canvas = $("#canvas");
+var context = canvas.getContext('2d');
+
+
+/*
+This function initializes the drawing state
+*/
+function init() {
+
+}
+
+/*
+This function handles the onmousedow event on the polygon
+draw button
+*/
+function drawPolyline() {
+    alert("hey2");
+    canvas.beginPath();
+    canvas.mousedown(function() {
+            alert("hey2");
+        });
+    
+}
+
+
+
+
+
+
