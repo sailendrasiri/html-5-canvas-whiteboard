2b4d6ad3dde6badf5878bffb18e150e8f3a578b7
kristoffer.snabb <kristoffer.snabb@2a9e65dc-1f86-11df-98cf-3116c4ba2102>
2010-03-10 17:47:56 +0000

    now it works, debugged and just added some console.log messages at the moment..

--- whiteboard.js
+++ whiteboard.js
@@ -1,29 +1,32 @@
 
 
-var canvas = $("#canvas");
+var canvas = document.getElementById("canvas");
+console.log(canvas);
 var context = canvas.getContext('2d');
 
+//initial values for the drawing context
+context.lineWidth = 5;
+context.lineCap = "round";
 
-/*
-This function initializes the drawing state
-*/
-function init() {
 
-}
 
 /*
 This function handles the onmousedow event on the polygon
 draw button
 */
 function drawPolyline() {
-    alert("hey2");
-    canvas.beginPath();
-    canvas.mousedown(function() {
-            alert("hey2");
-        });
-    
+    console.log("draw polyline begin");
+    context.beginPath();
+    $("#canvas").bind("mousedown", addPointToPath);
 }
 
+function addPointToPath(event) {
+    console.log("add point to path");
+    console.log(event.clientY);
+    console.log(event.clientX);
+    console.log(event.target.offsetTop);
+    console.log(event.target.offsetLeft);
+}
 
 
 
