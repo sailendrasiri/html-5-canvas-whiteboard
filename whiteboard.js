69d52b6f2a0214d762acd3393166bb4df2aeba2f
kristoffer.snabb <kristoffer.snabb@2a9e65dc-1f86-11df-98cf-3116c4ba2102>
2010-03-11 21:05:23 +0000

    drawing of popyline seems to work it just some coordinate counting problems.

--- whiteboard.js
+++ whiteboard.js
@@ -11,22 +11,46 @@ context.lineCap = "round";
 
 
 /*
+This function adds a point to the polyline 
+that is beeing drawn
+*/
+function addPointToPolyline(event) {
+    console.log("add point to path");
+    context.lineTo(event.layerX,event.layerY);
+    context.stroke();
+}
+
+/*
+This function stops the polyline drawing
+*/
+function endPolylineDraw(event) {
+    console.log("end polyline draw");
+    $("#canvas").unbind();
+}
+
+/*
+This function is called
+on mousedown when the polyline draw
+is chosen.
+*/
+function beginPolylineDraw(event) {
+    console.log("begin polyline draw");
+    context.moveTo(event.layerX,event.layerY);
+    $("#canvas").bind("mousemove", addPointToPolyline);
+    $("#canvas").bind("mouseup", endPolylineDraw);
+    $("#canvas").bind("mouseout", endPolylineDraw);
+}
+
+/*
 This function handles the onmousedow event on the polygon
 draw button
 */
 function drawPolyline() {
     console.log("draw polyline begin");
     context.beginPath();
-    $("#canvas").bind("mousedown", addPointToPath);
+    $("#canvas").bind("mousedown", beginPolylineDraw);
 }
 
-function addPointToPath(event) {
-    console.log("add point to path");
-    console.log(event.clientY);
-    console.log(event.clientX);
-    console.log(event.target.offsetTop);
-    console.log(event.target.offsetLeft);
-}
 
 
 
